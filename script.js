773445f8d67175ac53379cfcedf69482189d9b55
Sahan Chamara <sahanchamara456@gmail.com>
2024-12-14 14:53:58 +0530

    load Sections to items

--- script.js
+++ script.js
@@ -38,8 +38,11 @@ function signIn() {
     }
 }
 
-//IItem array
-let itemArr = [
+// all Item Array
+
+
+// Item Arrays
+let BurgersArr = [
     {
         itemCode: "B1001",
         itemNAme: "Classic Burger (Large)",
@@ -137,8 +140,10 @@ let itemArr = [
         price: 900.00,
         discount: 0,
         image: "Images/card burger - 1.jpg"
-    },
-    //submarines
+    }
+];
+
+let submarinesArr = [
     {
         itemCode: "B1015",
         itemNAme: "Crispy Chicken Submarine (Large)",
@@ -201,8 +206,10 @@ let itemArr = [
         price: 3000.00,
         discount: 0,
         image: "Images/card burger - 1.jpg"
-    },
-    //Fries
+    }
+];
+
+let friesArr = [
     {
         itemCode: "B1024",
         itemNAme: "Steak Fries (Large)",
@@ -244,8 +251,10 @@ let itemArr = [
         price: 600.00,
         discount: 0,
         image: "Images/card burger - 1.jpg"
-    },
-    //Pasta
+    }
+];
+
+let pastaArr = [
     {
         itemCode: "B1030",
         itemNAme: "Chicken n Cheese Pasta",
@@ -294,8 +303,10 @@ let itemArr = [
         price: 2000.00,
         discount: 0,
         image: "Images/card burger - 1.jpg"
-    },
-    //chicken
+    }
+];
+
+let chickenArr = [
     {
         itemCode: "B1037",
         itemNAme: "Fried Chicken (Small)",
@@ -337,8 +348,10 @@ let itemArr = [
         price: 2100.00,
         discount: 0,
         image: "Images/card burger - 1.jpg"
-    },
-    //Beverages
+    }
+];
+
+let beveragesArr = [
     {
         itemCode: "B1043",
         itemNAme: "Pepsi (330ml)",
@@ -367,15 +380,35 @@ let itemArr = [
         discount: 0,
         image: "Images/card burger - 1.jpg"
     }
-]
+];
+// ============================  Array End  =================================
+
+// cliclked Section Array declare by globaly...
+let clickSectionArr;
+
+function loadItems(clickSection) {    
+    //checking the click Section
+
+    if(clickSection=='Burger'){
+        clickSectionArr=BurgersArr;
+    }else if(clickSection=='Submarines'){
+        clickSectionArr=submarinesArr;
+    }else if(clickSection=='Fries'){
+        clickSectionArr=friesArr;
+    }else if(clickSection=='Pasta'){
+        clickSectionArr=pastaArr;
+    }else if(clickSection=='Chicken'){
+        clickSectionArr=chickenArr;
+    }else if(clickSection=='Beverages'){
+        clickSectionArr=beveragesArr;
+    }
 
 
-console.log(itemArr);
-let itemCards = document.getElementById("itemCards");
+    let itemCards = document.getElementById("itemCards");
 
-let setBody = "";
-itemArr.forEach(items => {
-    setBody += `<div class="newCard">
+    let setBody = "";
+    clickSectionArr.forEach(items => {
+        setBody += `<div class="newCard">
                 <div class="image_container">
                   <img src="${items.image}" alt="" class="image">                    
                 </div>
@@ -403,16 +436,50 @@ itemArr.forEach(items => {
                 </div>
             </div>`
 
-    itemCards.innerHTML = setBody;
+        itemCards.innerHTML = setBody;
+    });
 
+}
+// let itemCards = document.getElementById("itemCards");
 
-});
+// let setBody = "";
+// itemArr.forEach(items => {
+//     setBody += `<div class="newCard">
+//                 <div class="image_container">
+//                   <img src="${items.image}" alt="" class="image">                    
+//                 </div>
+//                 <div class="newTitle">
+//                     <span>${items.itemNAme}</span>
+//                 </div>                
+//                 <div class="action">
+//                     <div class="price">
+//                         <span>RS.${items.price}</span>
+//                     </div>                    
+//                     <button class="cartBtn" onclick="addCart('${items.itemCode}')">
+//                         <svg class="cart" fill="white" viewBox="0 0 576 512" height="1em"
+//                             xmlns="http://www.w3.org/2000/svg">
+//                             <path
+//                                 d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z">
+//                             </path>
+//                         </svg>
+//                         ADD TO CART
+//                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512" class="product">
+//                             <path
+//                                 d="M211.8 0c7.8 0 14.3 5.7 16.7 13.2C240.8 51.9 277.1 80 320 80s79.2-28.1 91.5-66.8C413.9 5.7 420.4 0 428.2 0h12.6c22.5 0 44.2 7.9 61.5 22.3L628.5 127.4c6.6 5.5 10.7 13.5 11.4 22.1s-2.1 17.1-7.8 23.6l-56 64c-11.4 13.1-31.2 14.6-44.6 3.5L480 197.7V448c0 35.3-28.7 64-64 64H224c-35.3 0-64-28.7-64-64V197.7l-51.5 42.9c-13.3 11.1-33.1 9.6-44.6-3.5l-56-64c-5.7-6.5-8.5-15-7.8-23.6s4.8-16.6 11.4-22.1L137.7 22.3C155 7.9 176.7 0 199.2 0h12.6z">
+//                             </path>
+//                         </svg>
+//                     </button>
+//                 </div>
+//             </div>`
+
+//     itemCards.innerHTML = setBody;
+// });
 
 // cart array
 let cartArr = [];
 
-
-function addCart(code) {    
+// Cart adding function
+function addCart(code) {
     let cartAdding = document.getElementById("cartAdding");
     let addArray = "";
 
@@ -421,38 +488,38 @@ function addCart(code) {
         input: "text",
         showCancelButton: true, // Optional: Adds a cancel button
         confirmButtonText: "OK", // Optional: Customizes the OK button text
-      }).then((result) => {
-        if (result.isConfirmed) {         
-          let qty = result.value   
-          
-      
-          // Call your custom function here
-          quantity(qty);
-        } else {
-          console.log("Action was cancelled or dismissed.");
-        }
-      });
-      
-      // Define your custom function
-      function handleOkButtonClick(quantity) {
-        alert("You entered: " + quantity);
-        // Add additional logic here as needed
-      }
-      
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let qty = result.value
 
-    for (let i = 0; i < itemArr.length; i++) {
-        if (code == itemArr[i].itemCode) {
+
+            // Call your custom function here
+            quantity(qty);
+        } else {
+            console.log("Action was cancelled or dismissed.");
+        }
+    });
+
+    // // Define your custom function
+    // function handleOkButtonClick(quantity) {
+    //     alert("You entered: " + quantity);
+    //     // Add additional logic here as needed
+    // }
+
+
+    for (let i = 0; i < clickSectionArr.length; i++) {
+        if (code == clickSectionArr[i].itemCode) {
             addArray = ` <div class="addedCart">
                             <div class="cartCard">
                                 <div class="icon">
-                                <img class="cartImg" src="${itemArr[i].image}" alt="">
+                                <img class="cartImg" src="${clickSectionArr[i].image}" alt="">
                                     
                                 </div>
                                 <div class="content">
-                                    <span class="cartTitle">${itemArr[i].itemNAme}</span>                                                                      
+                                    <span class="cartTitle">${clickSectionArr[i].itemNAme}</span>                                                                      
                                     <div class="actions">
                                         <div>
-                                            <h4 class="prise">RS.${itemArr[i].price}</h4>
+                                            <h4 class="prise">RS.${clickSectionArr[i].price}</h4>
                                         </div>
                                     </div>
                                 </div>
@@ -466,15 +533,15 @@ function addCart(code) {
                                 </button>
                             </div>
                         </div> `
-                    
+
         }
     }
     cartArr.push(addArray);
-    cartAdding.innerHTML+=addArray;
-    
+    cartAdding.innerHTML += addArray;
 
 
-// remove item in the 
+
+    // remove item in the 
 
 
 
